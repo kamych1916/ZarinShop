e95f70568d27ae89a18c1b1853b6823381e98f22
Rakhimov Kamol <34067263+kamych1916@users.noreply.github.com>
2020-10-24 18:27:56 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        ZarinShop
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <b-dropdown id="dropdown-grouped" dropright  text="Dropdown with group" class="m-2">
+        <b-dropdown-item-button>
+          Non-grouped Item
+        </b-dropdown-item-button>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-group id="dropdown-group-1" header="Group 1">
+          <b-dropdown-item-button>First Grouped item</b-dropdown-item-button>
+          <b-dropdown-item-button>Second Grouped Item</b-dropdown-item-button>
+        </b-dropdown-group>
+        <b-dropdown-group id="dropdown-group-2" header="Group 2">
+          <b-dropdown-item-button>First Grouped item</b-dropdown-item-button>
+          <b-dropdown-item-button>Second Grouped Item</b-dropdown-item-button>
+        </b-dropdown-group>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item-button>
+          Another Non-grouped Item
+        </b-dropdown-item-button>
+      </b-dropdown>
     </div>
   </div>
 </template>
